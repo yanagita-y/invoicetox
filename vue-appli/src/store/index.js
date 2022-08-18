fb4a4a66a4cb036a7a07fff6600427cb8ca5a714
yanagita-y <yosiphi@hotmail.com>
2022-08-18 20:18:13 +0900

    2022/8/18 DBのデータ更新のやり方を調べている段階で止まっている

--- vue-appli/src/store/index.js
+++ vue-appli/src/store/index.js
@@ -96,6 +96,78 @@ export default createStore({
                 // An error happened.
             });
         },
+
+
+// 必要データ入力後のsubmitをここに記載して動かしたい
+// Add a new document in collection "cities"
+// submit:function({commit}, {preliminary}){
+submit:function(preliminary){
+    console.log(preliminary);
+    const companyRef = doc(db, 'company', 'rgCBq5keifzGpzYp4yUA');
+    setDoc(companyRef, { name: '無限会社残業' }, { merge: true });
+    // commit('company', preliminary);
+},
+/*        submit:function({commit}, {company, caseName, deadline}){
+            const auth = getAuth();
+            signInWithEmailAndPassword(auth, userMailaddress, userPassword)
+            .then((response) => {
+                const getFireStoreData = async function(){                    
+                    const docRef = doc(db, "users", response.user.uid);
+                    const docSnap = await getDoc(docRef);
+                    if (docSnap.exists()) {
+                        console.log("Document data:", docSnap.data());
+                        const user={...docSnap.data(), 'UID':response.user.uid};
+                        commit('setUser', user);
+                    } else {
+                        console.log("No such document!");
+                    }
+                    //ダッシュボードに移動
+                    router.push('/dashboard');
+                };
+                getFireStoreData(response.user.uid);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+        },*/
+// submitここまで
+// サンプル
+// こんな形でDBの内容を記載する(?)
+/*
+const docData = {
+    // stringExample: "Hello world!",
+    // booleanExample: true,
+    // numberExample: 3.14159265,
+    // dateExample: Timestamp.fromDate(new Date("December 10, 1815")),
+    // arrayExample: [5, true, "hello"],
+    // nullExample: null,
+    alertdate: "",
+    case: data,
+    casemanager: "",
+    column1: "",
+    column2: "",
+    deadline: data,
+    money: "",
+    name: data,
+    needtime: "",
+    repeat: "",
+    tax: "",
+    
+
+    objectExample: {
+        a: 5,
+        b: {
+            nested: "foo"
+        }
+    }
+};
+await setDoc(doc(db, "data", "one"), docData);
+//
+*/
+
+
+
+
         updateWallet:async function({commit},{getValue, toUID}){
             try {
                 await runTransaction(db, async (transaction) => {
--- vue-appli/src/views/HomeView.vue
+++ vue-appli/src/views/HomeView.vue
@@ -1,27 +1,44 @@
 <template>
   <div class="signup">
-    <h1 v-if="flag" >新規登録画面</h1>
-    <h1 v-else>ログイン画面</h1>
+    <h1>新規登録</h1>
     <table>
-        <tr v-if="flag">
-          <td>ユーザー名：</td><td><input type="txt" v-model="name"></td>
+        <tr v-if="flag"></tr>
+<!-- 作成したデータベースの入力項目を作成 -->
+        <tr>
+          <td>相手名称：</td><td><input type="txt" v-model="company"></td>
         </tr>
         <tr>
-          <td>メールアドレス：</td><td><input type="email" v-model="mailaddress"></td>
+          <td>案件名：</td><td><input type="txt" v-model="caseName"></td>
+        </tr>
+        <!-- <tr>
+          <td>案件担当者名：</td><td><input type="txt" v-model="CaseManager"></td>
+        </tr> -->
+        <tr>
+          <td>締め切り：</td><td><input type="date" v-model="deadline"></td>
+        </tr>
+        <!-- <tr>
+          <td>アラートを出す日：</td><td><input type="txt" v-model="alertdate"></td>
         </tr>
         <tr>
-          <td>パスワード：</td><td><input type="password" v-model="password"></td>
+          <td>繰り返し：</td><td><input type="txt" v-model="repeat"></td>
         </tr>
+        <tr>
+          <td>金額：</td><td><input type="txt" v-model="money"></td>
+        </tr>
+        <tr>
+          <td>必要稼働時間：</td><td><input type="txt" v-model="needTime"></td>
+        </tr>
+        <tr>
+          <td>税率：</td><td><input type="txt" v-model="tax"></td>
+        </tr>
+        <tr>
+          <td>拡張1：</td><td><input type="txt" v-model="column1"></td>
+        </tr>
+        <tr>
+          <td>拡張2：</td><td><input type="txt" v-model="column2"></td>
+        </tr> -->
       </table>
-      <template v-if="!flag">
-        <button class="button is-primary" @click="login">ログイン</button>
-        <p @click="toggleFlag">新規登録はこちらから</p>
-      </template>
-      <template v-else>
-        <button @click="signs">新規登録</button>
-        <p @click="toggleFlag">ログインはこちらから</p>
-      </template>
-      <!-- <router-link to="/">ログインはこちらから</router-link> -->
+        <button class="button is-primary" @click="submit">送信</button>
   </div>
 </template>
 
@@ -36,16 +53,16 @@
       }
     },
     methods: {
-      signs: function () {
-        if(this.name && this.mailaddress && this.password){
-          this.$store.dispatch('sendAuth', { userName: this.name, userMailaddress: this.mailaddress, userPassword: this.password });
-        }
-      },
+      // signs: function () {
+      //   if(this.name && this.mailaddress && this.password){
+      //     this.$store.dispatch('sendAuth', { userName: this.name, userMailaddress: this.mailaddress, userPassword: this.password });
+      //   }
+      // },
       toggleFlag: function () {
         this.flag = !this.flag;
       },
-      login: function () {
-        this.$store.dispatch('signIn', { userMailaddress: this.mailaddress, userPassword: this.password });
+      submit: function () {
+        this.$store.dispatch('submit', { company: this.company});
       }
     }
   }
