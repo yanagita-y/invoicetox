6895ee3d09facfcbd1f8fa8fe19f862fab17ad5c
Kimura <>
2022-08-25 19:55:25 +0900

    8/25　共有パーツに日付を表示

--- vue-appli/src/App.vue
+++ vue-appli/src/App.vue
@@ -1,8 +1,21 @@
 <template>
+  <p style="text-align: right;">{{ today }}</p>
   <div id ="app">
   <router-view/>
   </div>
 </template>
 
 <script>
+  export default {
+    data(){
+      return {
+        today : ""
+      }
+    },
+    mounted: function(){
+      const now = new Date();
+      this.today = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() ;
+
+    }
+  }
 </script>
